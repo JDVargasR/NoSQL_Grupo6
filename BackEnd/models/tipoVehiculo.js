c955c5c3fd3139b9d4e9a591a5d6c5abf8c22ab5
JDVargasR <josedavargasr@gmail.com>
2025-08-07 02:38:21 -0600

    Organización Pertinente de archivos del Proyecto

--- BackEnd/models/tipoVehiculo.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require("mongoose");
-
-const vehiculoSchema = new mongoose.Schema({
-  tipo_vehiculo: { type: Number, required: true, unique: true },
-  id_estado: { type: Number, required: true, ref: "Estado" },
-});
-
-module.exports = mongoose.model("Vehiculo", vehiculoSchema);
